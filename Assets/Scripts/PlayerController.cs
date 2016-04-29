0ae2ac6ddcf3d49c250571eac84c107f5f90e0bf
malltaf <malltaf@malltaf.ru>
2016-04-29 16:03:03 +0300

    end of ball movement
    second

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    void FexedUpdate()
-    {
+    public float speed;
+    private Rigidbody rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
 
